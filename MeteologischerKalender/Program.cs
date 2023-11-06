b2797a5490e10438f951966a104f47452e46fe66
nake <oliver.nake@web.de>
2023-11-06 11:46:18 +0100

    Projektdateien hinzufügen.

---
+++ MeteologischerKalender/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeteologischerKalender
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Erstelle ein Programm, welches zu einem beliebigen
+            // Datum die Jahreszeit auf der Console ausgibt.
+        }
+    }
+}
